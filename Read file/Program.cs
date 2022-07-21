16241f2acb227cd4fce3efb5d4bbf491bcbbf73f
Vitalik <vitalikkravez1@gmail.com>
2022-07-21 12:28:25 +0300

    Add project files.

---
+++ Read file/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Read_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
